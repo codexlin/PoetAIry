c34a566d33c373510660cfdd5832080f16b2feb9
LinRenJie <xoxosos666@gmail.com>
2024-06-13 18:42:34 +0800

    refactor: 修改env.mjs

--- src/env.mjs
+++ src/env.mjs
@@ -1,11 +1,14 @@
-import { createEnv } from "@t3-oss/env-nextjs"
-import { z } from "zod"
+import { createEnv } from "@t3-oss/env-nextjs";
+import { z } from "zod";
 
+// 使用 createEnv 创建环境变量配置 通过 zod 验证
 export const env = createEnv({
+  // 定义客户端环境变量
   client: {
-    NEXT_PUBLIC_APP_URL: z.string().min(1),
+    NEXT_PUBLIC_APP_URL: z.string().min(1), // NEXT_PUBLIC_APP_URL 必须是非空字符串
   },
+  // 定义运行时环境变量
   runtimeEnv: {
-    NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
+    NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL, // 从 process.env 中获取 NEXT_PUBLIC_APP_URL
   },
-})
+});
