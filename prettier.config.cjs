0b792ddf52e895dd020ea27bf800e10cd52ac70c
LinRenJie <xoxosos666@gmail.com>
2024-06-13 13:46:22 +0800

    feat: config

---
+++ prettier.config.cjs
@@ -0,0 +1,10 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+  endOfLine: "lf",
+  tabWidth: 2,
+  printWidth: 80,
+  useTabs: false,
+  singleQuote: false,
+  semi: false,
+  plugins: ["prettier-plugin-tailwindcss"],
+}
